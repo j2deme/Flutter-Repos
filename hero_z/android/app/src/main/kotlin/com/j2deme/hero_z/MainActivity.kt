7d21a236041e1c509d984bf2bac65cfc876695c9
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-04-23 20:31:22 -0600

    Agrega uso del widget de animación Hero

---
+++ hero_z/android/app/src/main/kotlin/com/j2deme/hero_z/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.j2deme.hero_z
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ hero_z/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo Hero',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Demo Hero'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SegundaPantalla()),
+            );
+          },
+          child: Center(
+            child: Hero(
+              tag: 'avatar',
+              child: Image.asset(
+                'assets/avatar.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class SegundaPantalla extends StatelessWidget {
+  const SegundaPantalla({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Segunda Pantalla'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'avatar',
+            child: Image.asset(
+              'assets/avatar.png',
+              width: 400,
+              height: 400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
